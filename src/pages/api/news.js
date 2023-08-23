a748dd4d13c4f859c8d787713b1a2772217a6b7a
arifmia1129 <arifmia1129@gmail.com>
2023-08-23 20:17:56 +0600

    create first server api with next js. first of all create api folder into pages folder. after created api folder create file and declare function. function receive req, res. finally must be export default function

---
+++ src/pages/api/news.js
@@ -0,0 +1,9 @@
+const handler = (req, res) => {
+  res.status(200).json({
+    success: true,
+    message: "Successfully running server",
+  });
+};
+
+export default handler;
+// uvqB4u2MbOeaWifZ;
